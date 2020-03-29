6f31b3e78d4ac9e559d521ce9c4c64982e8b1f93
rkumarp <issac7@gmail.com>
2020-03-28 21:16:03 -0500

    now supports 2 keys

--- app.py
+++ app.py
@@ -7,29 +7,46 @@ class Repository(object):
     Now connects to local mongo
     """
 
-    def __init__(self, schema, collection):
+    def __init__(self, schema, collection, host=None):
         # create connection to Mongo for that schema
-        client = pymongo.MongoClient()
+        client = pymongo.MongoClient('localhost')
         db = client[schema]
         self.collection = db[collection]
 
-    def find(self):
-        return self.collection.find()
+    def find(self, x=None):
+        if isinstance(x, dict):
+            print('incoming dict')
+            return list(self.collection.find(x))
+        elif isinstance(x, str):
+            print('incoming str')
+            return self.collection.find_one({'_id': x})
+        else:
+            return self.collection.find()
 
-    def findOne(self, x):
-        return self.collection.find_one({'_id': x})
-
-    def deleteOne(self, x):
-        # del self.data[x]
-        result = self.collection.delete_one({'_id': x})
-        print(f'Deleted {x}')
+    def delete(self, x):
+        if isinstance(x, str):
+            result = self.collection.delete_one({'_id': x})
+            print(f'Deleted {x}')
+        elif isinstance(x, dict):
+            result = self.collection.delete_many(x)
+            print(f'Deleted {x}')
         return result
 
-    def updateOne(self, k, v):
+    def update(self, k, v):
         print(f'{k} updated to {v}')
-        result = self.collection.update_one(
-            {'_id': k}, {'$set': v}, upsert=True)
-        print(result.raw_result, result.modified_count, result.upserted_id)
+        if isinstance(k, dict):
+            print("dict")
+            result = self.collection.update_many(
+                k, {'$set': v}, upsert=True)
+            print(result.raw_result, result.modified_count, result.upserted_id)
+        elif isinstance(k, str):
+            print("str")
+            result = self.collection.update_one(
+                {'_id': k}, {'$set': v}, upsert=True)
+            print(result.raw_result, result.modified_count, result.upserted_id)
+
+        else:
+            result = {}
         return v
 
 
@@ -53,18 +70,22 @@ class MongoDict(collections.abc.MutableMapping):
 
     def __getitem__(self, key):
         print(f'Fetching key {key}')
-        return self.repository.findOne(key)
+        return self.repository.find(key)
 
     def __delitem__(self, key):
         print(f'Deleting key {key}')
-        return self.repository.deleteOne(key)
+        return self.repository.delete(key)
 
     def __setitem__(self, key, value):
         print(f'Setting {key} to {value}')
-        self.repository.updateOne(key, value)
+        self.repository.update(key, value)
 
     def __repr__(self):
         return f'{self.schema}:{self.collection} - {len(list(self.repository.find()))} documents'
+
+    def iterData(self):
+        for x in self.repository.find():
+            yield x
 
 
 if __name__ == "__main__":
